09cb17ef7f9a2276799d72e4b67e17c295d381e6
venkatesh-siva <sivakumar.ve@northeastern.edu>
2021-04-23 15:46:53 -0400

    adding query

--- src/main/java/com/csye7250/project/webapp/repository/PropertyRepository.java
+++ src/main/java/com/csye7250/project/webapp/repository/PropertyRepository.java
@@ -2,10 +2,15 @@ package com.csye7250.project.webapp.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.csye7250.project.webapp.entity.Property;
 
 public interface PropertyRepository extends CrudRepository<Property, Integer> {
 	List<Property> findByNodeId(Integer nodeId);
+	
+	@Query("SELECT p.techTerm FROM Property p JOIN Nodes n on n.nodeId = p.nodeId JOIN [Domain] d on d.dbName =n.dbName where d.dbName =:dbName")
+	List<Property> customFindProperty(@Param("dbName") String dbName);
 }
